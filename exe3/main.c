f3f4a52eb8ac0f40d093a6268cd4d1e2017b5e1c
MaluSevilha <marialssac@al.insper.edu.br>
2025-03-12 05:46:03 -0600

    exercício 3 funcionando

--- exe3/main.c
+++ exe3/main.c
@@ -6,6 +6,7 @@ const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
 volatile int flag_f_r = 0;
+volatile bool timer_fired = false;
 
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) { // fall edge
@@ -18,6 +19,11 @@ void btn_callback(uint gpio, uint32_t events) {
     }
 }
 
+int64_t alarm_callback(alarm_id_t id, void *user_data) {
+    timer_fired = true;
+    return 0;
+}
+
 int main() {
     stdio_init_all();
 
@@ -28,20 +34,31 @@ int main() {
     gpio_set_dir(BTN_PIN_R, GPIO_IN);
     gpio_pull_up(BTN_PIN_R);
 
+    gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &btn_callback);
+
+    alarm_id_t alarm;
     int led_status = 0;
 
-    gpio_set_irq_enabled_with_callback(
-        BTN_PIN_R, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &btn_callback);
-
     while (true) {
-        uint64_t before = to_ms_since_boot(get_absolute_time());
 
         if (flag_f_r) {
-            uint64_t now = to_ms_since_boot(get_absolute_time());
-            if (now - before > 500){
+            alarm = add_alarm_in_ms(500, alarm_callback, NULL, false);
+
+            while(!timer_fired){
+                sleep_ms(10);
+            }
+
+            timer_fired = false;
+
+            if (flag_f_r){
                 led_status = !led_status;
                 gpio_put(LED_PIN_R, led_status);
             }
+
+            while(flag_f_r){
+                sleep_ms(10);
+            }
+            
         }
     }
 }
